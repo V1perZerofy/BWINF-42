4c0478d6a1f7497c9c693e77165ab4576458c3ff
jademaennchen <nicolas.beninde@gmail.com>
2023-10-05 01:10:56 +0200

    Begin Input Parser

--- Problem3_Zauberschule.py
+++ Problem3_Zauberschule.py
@@ -1,1 +1,27 @@
-print("Hello World")
+import imaplib
+import random
+
+layout = []
+
+def is_valid_position(layout, x, y, z):
+    return layout[x][y][z] == "."
+
+def inputLayoutFromFile(file):
+    f = open(file, "r")
+    dimX, dimY = map(int, f.readline().split())
+    print(dimX)
+    print(dimY)
+    layout = [[[0 for _ in range(dimX)] for _ in range(dimY)] for _ in range(2)]
+    #print(layout)
+    for i in range(1):
+        for j in range(dimY - 1):
+            line = list(f.readline())
+            for k in range(dimX - 1):
+                layout[k][j][i] = line[k]
+                print(layout)
+        f.readline()
+    print(layout)
+
+
+
+inputLayoutFromFile("zauberschule0.txt")

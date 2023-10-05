9d1307b5c4c36e40869775977e0d127ddf2a4b34
jademaennchen <nicolas.beninde@gmail.com>
2023-10-05 19:44:41 +0200

    Global Variable Fix

--- Problem3_Zauberschule.py
+++ Problem3_Zauberschule.py
@@ -1,7 +1,7 @@
 import imaplib
 import random
 
-layout = [[[0 for _ in range(1)] for _ in range(1)] for _ in range(1)]
+layout = []
 startX = -1
 startY = -1
 startZ = -1
@@ -12,12 +12,13 @@ def is_valid_position(layout, x, y, z):
     return layout[x][y][z] == "."
 
 def inputLayoutFromFile(file):
+    global layout
     f = open(file, "r")
     dimX, dimY = map(int, f.readline().split())
     print(dimX)
     print(dimY)
     layout = [[[0 for _ in range(2)] for _ in range(dimY)] for _ in range(dimX)]
-    outputLayout()
+    print(layout)
     for i in range(2):
         for j in range(dimY):
             line = list(f.readline())
@@ -28,8 +29,9 @@ def inputLayoutFromFile(file):
                     startY = j
                     startZ = i
         f.readline()
-    outputLayout()
+    print(layout)
     print(startX, startY, startZ)
+    print(layout[0][0][0])
 
 def outputLayout():
     for i in range(2):
@@ -37,6 +39,9 @@ def outputLayout():
             for k in range(dimX):
                 print(layout[k][j][i])
 
-inputLayoutFromFile("zauberschule0.txt")
-outputLayout()
-print(layout[0][0][0])
+if __name__ == '__main__':
+    inputLayoutFromFile("zauberschule0.txt")
+    #outputLayout()
+    print(layout[0][0][0])
+    print(layout)
+

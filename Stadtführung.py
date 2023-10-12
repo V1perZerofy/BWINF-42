c57f852673fac90ec7ccf3588667cf395eda64ad
V1perZerofy <86120394+V1perZerofy@users.noreply.github.com>
2023-10-12 14:51:40 +0200

    Little bit Stadtführung

--- Stadtführung.py
+++ Stadtführung.py
@@ -1,16 +1,19 @@
 def inputFile(filename):
-    #first line of input file is n
-    #other lines have to be split into a 2d array of strings 
-    #each line is split at ,
     with open(filename, 'r') as f:
         n = int(f.readline().strip())
         #split each line at , and append to list
         construction = [f.readline().strip().split(',') for _ in range(n)]
     return n, construction
 
-def printConstruction(n, construction):
+def printConstruction(n, construction):   
+    
     for i in range(n):
         print(construction[i])
+
+def interpretConstruction(n, construction):
+    for i in range(n):
+        if construction[i][2] == ' ':
+
 
 n, construction = inputFile("Input/tour1.txt")
 printConstruction(n, construction)

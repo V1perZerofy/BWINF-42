c46bef9e4e546ebcc7fb312781dbf1fc74b82c41
jademaennchen <nicolas.beninde@gmail.com>
2023-10-18 22:44:24 +0200

    Why so many bugs in Die Goldene Mitte

--- Aufgabe_2/DieGoldeneMitte.py
+++ Aufgabe_2/DieGoldeneMitte.py
@@ -1,18 +1,46 @@
 #BWINF Runde_1 Aufgabe_2
 import numpy as np
 import math
+finished = False
 
 
 def inputCubeFromFile(file):
     f = open(file, "r")
     dimX, dimY, dimZ = map(int, f.readline().strip().split())
-    cube = np.array([[['' for _ in range(dimZ)] for _ in range(dimY)] for _ in range(dimX)])
+    cube = np.array([[["" for _ in range(dimZ)] for _ in range(dimY)] for _ in range(dimX)])
     cube[math.floor(dimX / 2), math.floor(dimY / 2), math.floor(dimZ / 2)] = "G"
     numberOfPieces = int(f.readline().strip())
     pieces = []
     for _ in range(numberOfPieces):
         pieces.append(f.readline().strip().split())
-    return cube, dimX, dimY, dimZ, pieces, numberOfPieces
+    return cube, pieces, dimX, dimY, dimZ, numberOfPieces
+
+def isValidPosition(cube, dimX, dimY, dimZ, x, y, z):
+    if(0 <= x < dimX and 0 <= y < dimY and 0 <= z < dimZ):
+        return cube[x][y][z] == ""
+    return False
+
+def inputPiece(cube, pieces, dimX, dimY, dimZ, pieceNumber):
+    printCube(cube)
+    global finished
+    directions = [(pieces[pieceNumber][0], pieces[pieceNumber][1], pieces[pieceNumber][2]), (pieces[pieceNumber][0], pieces[pieceNumber][2], pieces[pieceNumber][1]), 
+                (pieces[pieceNumber][1], pieces[pieceNumber][0], pieces[pieceNumber][2]), (pieces[pieceNumber][1], pieces[pieceNumber][2], pieces[pieceNumber][0]), 
+                (pieces[pieceNumber][2], pieces[pieceNumber][0], pieces[pieceNumber][1]), (pieces[pieceNumber][2], pieces[pieceNumber][1], pieces[pieceNumber][0])]
+    print(directions)
+    for k in range(dimZ):
+        for j in range(dimY):
+            for i in range(dimX):
+                #for direction in directions:
+                if(isValidPosition(cube, dimX, dimY, dimZ, i, j, k)):
+                    cube[i][j][k] = pieceNumber
+                    if(pieceNumber == 7):
+                        return cube, pieceNumber
+                    inputPiece(cube, pieces, dimX, dimY, dimZ, pieceNumber + 1)
+                    if(pieceNumber == 7):
+                        return cube, pieceNumber
+
+    return cube, pieceNumber
+
 
 def printCube(cube):
     for i in range(3):
@@ -21,8 +49,10 @@ def printCube(cube):
         print("")
 
 if __name__ == '__main__':
-    cube, dimX, dimY, dimZ, pieces, numberOfPieces = inputCubeFromFile("Aufgabe_2/input/raetsel1.txt")
+    cube, pieces, dimX, dimY, dimZ, numberOfPieces = inputCubeFromFile("Aufgabe_2/input/raetsel1.txt")
     print(dimX, dimY, dimZ)
     printCube(cube)
     print(numberOfPieces)
     print(pieces)
+    cube, pieceNumber = inputPiece(cube, pieces, dimX, dimY, dimZ, 0)
+    printCube(cube)
